174df4fbf0357f9d1d03808495ab0acb038a03e1
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-10 20:00:59 -0600

    Adds the row__posters styling to account for horizonal row scrolling.

--- src/Row.js
+++ src/Row.js
@@ -1,27 +1,42 @@
-import React,  { useState, useEffect } from 'react';
-import axios from './axios';
+import React, { useState, useEffect } from "react";
+import axios from "./axios";
+import "./Row.css";
 
-function Row({ title, fetchURL}) {
-    const [movies, setMovies] = useState([]);
+const base_url = "https://image.tmdb.org/t/p/original/";
 
-    useEffect(() => {
-        async function fetchData() {
-            const request = await axios.get(fetchURL);
-            console.log(request);
-            setMovies(request.data.results);
-            return request;
-        }
-        fetchData();
-    }, [fetchURL]);
+function Row({ title, fetchURL }) {
+  const [movies, setMovies] = useState([]);
 
-    console.log(movies);
+  useEffect(() => {
+    async function fetchData() {
+      const request = await axios.get(fetchURL);
+      console.log(request);
+      setMovies(request.data.results);
+      return request;
+    }
+    fetchData();
+  }, [fetchURL]);
 
-    return (
-        <div>
-            <h2>{title} </h2>
-            { /* container -> posters */}
-        </div>
-    )
+  console.log(movies);
+
+  return (
+    <div className="row">
+      <h2>{title} </h2>
+
+      <div className="row__posters">
+        {/* Row posters */}
+        {movies.map((movie) => (
+          <img
+            key={movie.id}
+            className="row__poster"
+            src={`${base_url}${movie.poster_path}`}
+            alt={movie.name}
+          />
+        ))}
+      </div>
+      {/* container -> posters */}
+    </div>
+  );
 }
 
 export default Row;
