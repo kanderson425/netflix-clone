a1be0f6bc4beb9440392320357d4a29571945a67
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-10 21:01:20 -0600

    Adds majority of banner styling and page background color. Still requires NavBar component and additional color styling.

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React from "react";
 import "./App.css";
 import Row from "./Row";
+import Banner from "./Banner";
 import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
-      <h1>Netflix Clone</h1>
+      {/* Nav Bar */}
+      <Banner />
       <Row
         title="NETFLIX ORIGINALS"
         fetchURL={requests.fetchNetflixOriginals}
--- src/Row.js
+++ src/Row.js
@@ -10,14 +10,11 @@ function Row({ title, fetchURL, isLargeRow }) {
   useEffect(() => {
     async function fetchData() {
       const request = await axios.get(fetchURL);
-      console.log(request);
       setMovies(request.data.results);
       return request;
     }
     fetchData();
   }, [fetchURL]);
-
-  console.log(movies);
 
   return (
     <div className="row">
