05ea1d2964d60cc6b6e122ab400b5e7d870977d5
kanderson425 <kyleanderson.dev@gmail.com>
2020-11-10 20:08:59 -0600

    Adds all film type rows.

--- src/App.js
+++ src/App.js
@@ -1,15 +1,23 @@
-import React from 'react';
-import './App.css';
-import Row from './Row';
-import requests from './requests';
+import React from "react";
+import "./App.css";
+import Row from "./Row";
+import requests from "./requests";
 
 function App() {
   return (
     <div className="App">
       <h1>Netflix Clone</h1>
-      <Row title="NETFLIX ORIGINALS" fetchURL={requests.fetchNetflixOriginals}/>
-      <Row title="Trending Now" fetchURL={requests.fetchTrending}/>
-
+      <Row
+        title="NETFLIX ORIGINALS"
+        fetchURL={requests.fetchNetflixOriginals}
+      />
+      <Row title="Trending Now" fetchURL={requests.fetchTrending} />
+      <Row title="Top Rated" fetchURL={requests.fetchTopRated} />
+      <Row title="Action Movies" fetchURL={requests.fetchActionMovies} />
+      <Row title="Comedy Movies" fetchURL={requests.fetchComedyMovies} />
+      <Row title="Horror Movies" fetchURL={requests.fetchHorroMovies} />
+      <Row title="Romance Movies" fetchURL={requests.fetchRomanceMovies} />
+      <Row title="Documentaries" fetchURL={requests.fetchDocumentaries} />
     </div>
   );
 }
